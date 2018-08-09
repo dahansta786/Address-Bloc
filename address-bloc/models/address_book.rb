20db312c2b8c831258e0247ad06ba4f2d2c83f2c
Hani Anjarwala <hani.anjarwala@gmail.com>
2018-08-09 15:53:28 -0500

    checkpoint CSVs completed

--- address-bloc/models/address_book.rb
+++ address-bloc/models/address_book.rb
@@ -1,5 +1,6 @@
 # #8
 require_relative 'entry'
+require "csv"
 
     class AddressBook
         attr_reader :entries
@@ -8,6 +9,7 @@ require_relative 'entry'
             @entries = []
         end
     end
+
     def remove_entry(name, phone_number, email)
         delete_entry = nil
         
@@ -32,5 +34,17 @@ require_relative 'entry'
         end
         # #11
         entries.insert(index, Entry.new(name, phone_number, email))
+    end
+
+    # #7 Define import_from_csv. The method starts by reading the file, using File.read. The file will be in a CSV format. We use the CSV class to parse the file. The result of CSV.parse is an object of type CSV::Table
+    def import_from_csv(file_name)
+        # Implementation goes here
+        csv_text = File.read(file_name)
+        csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
+        # #8  Iterate over the CSV::Table object's rows. On the next line we create a hash for each row. We convert each row_hash to an Entry by using the add_entry method which will also add the Entry to the AddressBook's entries.
+        csv.each do |row|
+            row_hash = row.to_hash
+            add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
+        end
     end
 end
